f9354e9eb86e4e4cd1a8fd87d7dbc115483d5f6e
Fatma Laribi <82259973+fatma-laribi@users.noreply.github.com>
2023-12-05 22:18:58 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-   features {}
+  features {}
 }
 
 resource "azurerm_resource_group" "example" {
@@ -15,12 +15,53 @@ resource "azurerm_container_group" "example" {
 
   container {
     name   = "example-container"
-    image  = "fatmal/jenkinstest:latest"  # Use the desired Docker image
+    image  = "fatmal/jenkinstest:latest"
     cpu    = "0.5"
     memory = "1.5"
 
     ports {
       port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  container {
+    name   = "prometheus-container"
+    image  = "prom/prometheus:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 9090
+      protocol = "TCP"
+    }
+
+    // Provisioner to dynamically generate Prometheus configuration
+    provisioner "local-exec" {
+      command = <<EOT
+cat <<EOF > /tmp/prometheus.yml
+global:
+  scrape_interval: 15s
+  evaluation_interval: 15s
+
+scrape_configs:
+  - job_name: 'example-app'
+    static_configs:
+      - targets: ['localhost:80']
+EOF
+docker cp /tmp/prometheus.yml aci-example-container-group:/etc/prometheus/prometheus.yml
+EOT
+    }
+  }
+
+  container {
+    name   = "grafana-container"
+    image  = "grafana/grafana:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    ports {
+      port     = 3000
       protocol = "TCP"
     }
   }
