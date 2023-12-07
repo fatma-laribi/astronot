2fb94fd7dbd1625cb4ba1f5cd77ead9c6a7934de
Fatma Laribi <82259973+fatma-laribi@users.noreply.github.com>
2023-12-07 09:21:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,10 +71,6 @@ resource "azurerm_public_ip" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   allocation_method   = "Static"
-
-  tags = {
-    environment = "testing"
-  }
 }
 
 output "public_ip_address" {
